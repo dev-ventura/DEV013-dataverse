2b2c9fcd4ad28fccffb525b0268b57f797db1bf2
Melany Ventura <melanymarisolventura@gmail.com>
2024-01-25 13:22:24 -0400

    Estructura HTML, CSS, y JS renderizado del dataset

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './dataFunctions.js';
+//import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-console.log(example, renderItems(data), data);
+//console.log(example, renderItems(data), data);
+// Renderizar los elementos y agregarlos al cuerpo del documento
+const rootElement = document.querySelector('#root');
+// aqui renderizamos los item sacados del dataset con la funcion que creamos en view y lo pasamos a html
+rootElement.appendChild(renderItems(data));
--- src/view.js
+++ src/view.js
@@ -1,5 +1,40 @@
 export const renderItems = (data) => {
-  console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+  // Creamos un contenedor para los elementos renderizados
+  const container = document.createElement('ul');
+
+  // Iteraremos sobre cada objeto en el dataset
+  data.forEach(item => {
+    // Creamos un objeto contenedor para cada item (tarjeta)
+    const itemContainer = document.createElement('li');
+    itemContainer.classList.add('lista')
+    itemContainer.setAttribute('itemtype', 'http://schema.org/Person')
+    // Creamos una imagen DOM y establecemos la fuente (imagenURL)
+    const imageUrl = document.createElement('img');
+    imageUrl.src = item.imageUrl;
+    // Creamos un objeto para mostrar el número de episodios y genero
+    const descriptionTotal = document.createElement('p');
+    //const genreTotal = document.createElement('p');
+    //const subtitleTotal = document.createElement('p');
+    descriptionTotal.textContent = `${item.facts.cantidadEpisodios} Ep. | ${item.facts.totalGenero} | Sub. ${item.facts.totalSubtitulos}`;
+    //genreTotal.textContent = `${item.facts.totalGenero}`;
+    // Creamos un objeto para mostrar el nombre del k-drama
+    const nameKdrama = document.createElement('h3');
+    nameKdrama.textContent = item.name;
+    // Agregar los elementos al contenedor del item
+    itemContainer.appendChild(imageUrl);
+    itemContainer.appendChild(descriptionTotal);
+    //itemContainer.appendChild(genreTotal);
+    itemContainer.appendChild(nameKdrama);
+    //itemContainer.appendChild(subtitleTotal);
+
+    // Agregar el contenedor del item al contenedor principal
+    container.appendChild(itemContainer);
+  });
+
+  // Devolver el contenedor principal con todos los elementos renderizados
+  return container;
 };
+
+
+
