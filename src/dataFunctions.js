63462a6ab482ce8557d5bcf816dfc1d7744a1e1c
Antone Gonzales <es.angelika@gmail.com>
2024-01-26 14:44:01 -0500

    Prueba y error Intente hacer cambios en la funcion

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -3,11 +3,25 @@ export const filtrarSubtitulos = (data, idioma) => {
   // Verificar que el idioma sea válido (coreano, inglés, español)
   if (idioma === "Coreano" || idioma === "Inglés" || idioma === "Español") {
     // Filtrar kdramas según el idioma de los subtítulos
-    const kdramasFiltrados = data.filter(item => item.facts.totalSubtitulos === idioma);
-    //console.log(kdramasFiltrados)
+    const kdramasFiltrados = data.filter(it => item.facts.totalSubtitulos === idioma);
+    console.log(kdramasFiltrados)
     return kdramasFiltrados;
   } else {
     // Si no es ninguno de esos 3 idiomas , mantener las mismas tarjetas sin cambios
+    return data;
+  }
+}
+
+export const filtrarGenero = (data, genero) => {
+  // verificando que el genero sea valido (romance, drama, comedia, fantasia, thriller, accion)
+  if (genero === "Romance" || genero === "Drama" || genero === "Comedia" || genero === "Fantasía" || genero === "Thriller" || genero === "Accion") {
+    // filtrando los kdramas por el genero
+    const generoFiltrado = data.filter(item => {
+      item.facts.totalGenero === genero;
+    });
+    return generoFiltrado;
+  } else {
+    // en caso de que no exista ese filtro muestra todos los kdramas
     return data;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 //importando funciones
+
+import { filtrarGenero } from './dataFunctions.js';
 import { filtrarSubtitulos } from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
@@ -10,22 +12,27 @@ const rootElement = document.querySelector('#root');
 rootElement.appendChild(renderItems(data));
 
 // aqui invocamos a las opciones de seleccion por genero
-const selectGenre = document.querySelector("select[data-testid=select-filter]");
-// evento change 
+const selectGenre = document.querySelector("select[name=genero]");
 selectGenre.addEventListener("change", (event) => {
-    console.log(event.target.value);
+  const generoFiltrado = event.target.value;
+  console.log(generoFiltrado);
+  const kdramasFiltrado = filtrarGenero(data, generoFiltrado)
+  console.log(kdramasFiltrado);
+  rootElement.innerHTML = "";
+  rootElement.appendChild(renderItems(kdramasFiltrado));
 })
+
 // por subtitulos
 const selectSubtitle = document.querySelector("select[name=subtitle]");
 selectSubtitle.addEventListener("change", (event) => {
-    const idiomaSeleccionado = event.target.value;
-    //llamo a la funcion de filtrar Subtitulos 
-    const kdramasFiltrados = filtrarSubtitulos(data, idiomaSeleccionado)
-    rootElement.innerHTML = ""; // Limpiar el contenedor root
-    rootElement.appendChild(renderItems(kdramasFiltrados));
+  const idiomaSeleccionado = event.target.value;
+  //llamo a la funcion de filtrar Subtitulos 
+  const kdramasFiltrados = filtrarSubtitulos(data, idiomaSeleccionado)
+  rootElement.innerHTML = ""; // Limpiar el contenedor root
+  rootElement.appendChild(renderItems(kdramasFiltrados));
 })
 // aqui invocamos a las opciones para ordenar de asc y desc
 const order = document.querySelector("select[data-testid=select-sort]");
 order.addEventListener("change", (event) => {
-    console.log(event.target.value);
+  console.log(event.target.value);
 })
