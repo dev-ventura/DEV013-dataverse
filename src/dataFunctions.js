cde7fac21e064331c37b4207ab006938b8462bf3
Antone Gonzales <es.angelika@gmail.com>
2024-01-26 18:09:57 -0500

    Modifique la funcion de filtrado en Main.Js y data Ahora se puede filtrar por genero y sobre eso filtar por idioma. te deje comentarios
    modified# Please enter the commit message for your changes. Lines starting

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,11 +1,11 @@
-// Funcion para filtrar la data 
-export const filterData = (data, filterBy , value) => {
-    // Filtrar kdramas según el idioma de los subtítulos
-    const kdramasFiltrados = data.filter(item => item.facts[filterBy] === value);
-    console.log(kdramasFiltrados)
-    return kdramasFiltrados;
-    // cambios 
- }
+//Funcion para filtrar la data 
+export const filterData = (data, filterBy, value) => {
+  //Filtrar kdramas según el idioma de los subtítulos
+  const kdramasFiltrados = data.filter(item => item.facts[filterBy] === value);
+  //console.log(kdramasFiltrados)
+  return kdramasFiltrados;
+  // cambios 
+}
 
 
 // Funcion para ordenar de manera descendente
