70fc6b9b3d50c7e03ff1f0cb4b77c1e8894e4e43
Antone Gonzales <es.angelika@gmail.com>
2024-01-29 10:03:02 -0500

    agregue un comentario en main.js sobre el dom

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const rootElement = document.querySelector('#root');
 // aqui renderizamos los item sacados del dataset con la funcion que creamos en view y lo pasamos a html
 rootElement.appendChild(renderItems(data));
 
-// DOM
+// manipulacion del DOM
 const selectGenre = document.querySelector("select[name=genero]");
 const selectSubtitle = document.querySelector("select[name=subtitle]");
 const order = document.querySelector("select[name=ordenar]");
