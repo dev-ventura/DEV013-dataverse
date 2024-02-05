5d000a643e0de5f447aa31942ace0e6f7d16da08
Antone Gonzales <es.angelika@gmail.com>
2024-02-05 15:30:35 -0500

    Agregue el test unitario para la funcion filtrar

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -17,3 +17,36 @@ describe('sortData function', () => {
     expect(resultado).toEqual(sortData2);
   });
 });
+// test unitario para la funcion filtrar
+describe('filterData por Genero de Prueba ', () => {
+  const filter1 = [fakeData[0]];
+  it('Deberia filtrar por genero Romance', () => {
+    const result = filterData(fakeData, 'totalGenero', 'Romance');
+    expect(result).toStrictEqual(filter1);
+  });
+  const filter2 = [fakeData[1]];
+  it('Deberia filtrar por genero Drama', () => {
+    const result = filterData(fakeData, 'totalGenero', 'Drama');
+    expect(result).toStrictEqual(filter2);
+  });
+  const filter3 = [fakeData[2]];
+  it('Deberia filtrar por genero Comedia', () => {
+    const result = filterData(fakeData, 'totalGenero', 'Comedia');
+    expect(result).toStrictEqual(filter3);
+  });
+  const filter4 = [fakeData[3]];
+  it('Deberia filtrar por genero Fantasía', () => {
+    const result = filterData(fakeData, 'totalGenero', 'Fantasía');
+    expect(result).toStrictEqual(filter4);
+  });
+  const filter5 = [fakeData[4]];
+  it('Deberia filtrar por genero Thriller', () => {
+    const result = filterData(fakeData, 'totalGenero', 'Thriller');
+    expect(result).toStrictEqual(filter5);
+  });
+  const filter6 = [fakeData[5]];
+  it('Deberia filtrar por genero Acción', () => {
+    const result = filterData(fakeData, 'totalGenero', 'Acción');
+    expect(result).toStrictEqual(filter6);
+  });
+});
